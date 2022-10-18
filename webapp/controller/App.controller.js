4fc5e2b9abfaff52f3cdd8b8ec2e8cc43e82b6c1
supawutt <116049434+supawutt@users.noreply.github.com>
2022-10-18 13:56:29 +0700

    Add files via upload

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("ExpenseOnline.expenseonline.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
